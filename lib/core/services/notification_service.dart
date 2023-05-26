4f8121b200260534b778f83d51616d63a7278e43
vinihzets <viniciusdaarky@outlook.com>
2023-05-26 13:41:09 -0300

    implementado servico de notificacao

---
+++ lib/core/services/notification_service.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class CustomNotification {
+  final int id;
+  final String title;
+  final String body;
+  final String? payload;
+  final RemoteMessage? remoteMessage;
+
+  CustomNotification({
+    required this.id,
+    required this.title,
+    required this.body,
+    this.payload,
+    this.remoteMessage,
+  });
+}
+
+class NotificationService {
+  late FlutterLocalNotificationsPlugin localNotificationsPlugin;
+  late AndroidNotificationDetails androidDetails;
+
+  showLocalNotification(CustomNotification notification) {
+    localNotificationsPlugin.show(
+      notification.id,
+      notification.title,
+      notification.body,
+      NotificationDetails(
+        android: androidDetails,
+      ),
+      // payload: notification.payload,
+    );
+  }
+}
